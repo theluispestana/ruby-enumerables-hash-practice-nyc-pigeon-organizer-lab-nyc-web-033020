0e9a1c7114bb44bb4e6cd606a1b6551a7966d888
Luis Pestana <lpestana@protonmail.com>
2020-03-02 19:53:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,40 +10,20 @@ def nyc_pigeon_organizer(data)
   while index < names.length do
     name = names[index]
     new_pigeon_data[name] = {}
-    new_pigeon_data[name][:color] = color_organizer(name, data[:color])
-    new_pigeon_data[name][:gender] = color_organizer(name, data[:gender])
-    new_pigeon_data[name][:lives] = color_organizer(name, data[:lives])
+    new_pigeon_data[name][:color] = bird_organizer(name, data[:color])
+    new_pigeon_data[name][:gender] = bird_organizer(name, data[:gender])
+    new_pigeon_data[name][:lives] = bird_organizer(name, data[:lives])
     index += 1
   end
   new_pigeon_data
 end
 
-def color_organizer(bird_name, color_hash)
-  bird_colors = []
-  color_hash.each_pair do |hue, name|
+def bird_organizer(bird_name, hash)
+  bird_info = []
+  hash.each_pair do |key, name|
     if name.include?(bird_name)
-      bird_colors << "#{hue}"
+      bird_info << "#{key}"
     end
   end
-  bird_colors
-end
-
-def gender_organizer(bird_name, gender_hash)
-  bird_gender = []
-  gender_hash.each_pair do |gender, name|
-    if name.include?(bird_name)
-      bird_colors << "#{gener}"
-    end
-  end
-  bird_gender
-end
-
-def home_organizer(bird_name, home_hash)
-  bird_home = []
-  home_hash.each_pair do |home, name|
-    if name.include?(bird_name)
-      bird_colors << "#{home}"
-    end
-  end
-  bird_home
+  bird_info
 end
