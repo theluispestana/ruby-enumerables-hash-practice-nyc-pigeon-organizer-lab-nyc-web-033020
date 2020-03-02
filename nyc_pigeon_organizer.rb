2b1f737be07b33bf8d3343ea700d86e531bad9be
Luis Pestana <lpestana@protonmail.com>
2020-03-02 19:49:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,13 +5,45 @@ def nyc_pigeon_organizer(data)
   names.flatten!
   names.uniq!
 
-  pigeon_data = {}
+  new_pigeon_data = {}
   index = 0
   while index < names.length do
-    pigeon_data[names[index]][:color] = data[:color].select{ |key, value| value == names[index] }
+    name = names[index]
+    new_pigeon_data[name] = {}
+    new_pigeon_data[name][:color] = color_organizer(name, data[:color])
+    new_pigeon_data[name][:gender] = color_organizer(name, data[:gender])
+    new_pigeon_data[name][:lives] = color_organizer(name, data[:lives])
     index += 1
   end
-  puts pigeon_data
-  pp pigeon_data
-  pp names
+  new_pigeon_data
+end
+
+def color_organizer(bird_name, color_hash)
+  bird_colors = []
+  color_hash.each_pair do |hue, name|
+    if name.include?(bird_name)
+      bird_colors << "#{hue}"
+    end
+  end
+  bird_colors
+end
+
+def gender_organizer(bird_name, gender_hash)
+  bird_gender = []
+  gender_hash.each_pair do |gender, name|
+    if name.include?(bird_name)
+      bird_colors << "#{gener}"
+    end
+  end
+  bird_gender
+end
+
+def home_organizer(bird_name, home_hash)
+  bird_home = []
+  home_hash.each_pair do |home, name|
+    if name.include?(bird_name)
+      bird_colors << "#{home}"
+    end
+  end
+  bird_home
 end
